95e7a78bae2d6e26b9d758b44ddc50552df16f19
Ariarzg <Ariarzg@gmail.com>
2025-12-13 08:31:10 +0330

    Blog Id Route Implemented

---
+++ app/(shared-layout)/blog/[postId]/page.tsx
@@ -0,0 +1,67 @@
+import { buttonVariants } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { fetchQuery } from "convex/nextjs";
+import { ArrowLeft } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+interface postIdRouteProps {
+  params: Promise<{
+    postId: Id<"posts">;
+  }>;
+}
+
+const PostIdRoute = async ({ params }: postIdRouteProps) => {
+  const { postId } = await params;
+
+  const post = await fetchQuery(api.posts.getPostById, { postId: postId });
+
+  if (!post) {
+    return (
+      <h1 className="text-6xl font-extrabold text-red-500">No Post Found</h1>
+    );
+  }
+
+  return (
+    <div className="max-w-3xl mx-auto py-8 px-4 animate-in fade-in duration-500 relative">
+      <Link
+        href={"/blog"}
+        className={buttonVariants({ variant: "outline", className: "mb-4" })}
+      >
+        <ArrowLeft className="size-4" />
+        Back to blog
+      </Link>
+
+      <div className="relative w-full h-[400px] mb-8 rounded-xl overflow-hidden shadow-sm">
+        <Image
+          src={post.imageUrl ?? "/default_post_bg.jpeg"}
+          alt={post.title}
+          fill
+          className="object-cover hover:scale-105 transition-transform duration-500"
+        />
+      </div>
+
+      <div className="space-y-4 flex flex-col">
+        <h1 className="text-4xl font-bold tracking-tight text-foreground">
+          {post.title}
+        </h1>
+
+        <p className="text-sm text-muted-foreground">
+          Posted on: {new Date(post._creationTime).toLocaleDateString("en-DE")}
+        </p>
+      </div>
+
+      <Separator className="my-8" />
+
+      <p className="text-lg leading-relaxed text-foreground/90 whitespace-pre-wrap">
+        {post.body}
+      </p>
+
+      <Separator className="my-8" />
+    </div>
+  );
+};
+
+export default PostIdRoute;
--- app/(shared-layout)/blog/page.tsx
+++ app/(shared-layout)/blog/page.tsx
@@ -38,10 +38,7 @@ const Posts = async () => {
     <Card key={post._id} className="pt-0 overflow-hidden">
       <div className="relative h-48 w-full overflow-hidden">
         <Image
-          src={
-            post.imageUrl ??
-            "https://images.unsplash.com/photo-1764534513435-0ce1b310db63?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          }
+          src={post.imageUrl ?? "/default_post_bg.jpeg"}
           alt="wallpaper"
           fill
           className="object-cover"
